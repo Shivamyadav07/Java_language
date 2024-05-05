c8bd93746a0ba650fd90c8f67fe269ba10686c24
Shivamyadav07 <shivamvnsindia@gmail.com>
2024-05-05 23:44:32 +0530

    added some comments for better redability

--- GussMyNumber.java
+++ GussMyNumber.java
@@ -2,11 +2,16 @@ import java.util.*;
 
 public class GussMyNumber {
     public static void main(String[] args){
+
+        //Generating a random number
         int myNumber=(int)(Math.random()*100);
+        
+        //taking the guess number from user
         Scanner sc= new Scanner(System.in);
         int userNumber;
         System.out.println("To guess the number Enter a number between 1 to 100" + "\n" + "To stop enter -1 ");
         userNumber=sc.nextInt();
+
         while(userNumber!=-1){
             if(userNumber>myNumber){
                 System.out.println("Sorry, You gussed a heigher number");
@@ -19,8 +24,9 @@ public class GussMyNumber {
             System.out.println("To guess the number Enter a number between 1 to 100" + "\n" + "To stop enter -1");
             userNumber=sc.nextInt();
         }
+        
         if(userNumber==-1){
-            System.out.println("You were so close"+"The number is : " + myNumber);
+            System.out.println("You were so close"+ " the number was : " + myNumber);
         }else{
             System.out.println("The number is : " + myNumber);
         }
